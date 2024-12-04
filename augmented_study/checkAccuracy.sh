72b21a061b52ac304baccce4c835396be2e114f9
Ruhlen <ruhlensp@stu.cs.jmu.edu>
2024-12-04 10:46:38 -0500

    stuff

--- augmented_study/checkAccuracy.sh
+++ augmented_study/checkAccuracy.sh
@@ -1,8 +1,14 @@
 python genGraph.py
 
-python ../approx_solution/cs412_tsp_approx.py
+echo "Starting Abby's Code now"
+python ../approx_solution/cs412_tsp_approx.py < graph > abby.txt
 
-python ../exact_solution/cs412_tsp_exact.py
+echo "Starting Mateo's code now"
+python ../exact_solution/cs412_tsp_exact.py < graph > mateo.txt
+
+echo "Run Complete"
+
+echo "Plotting Results"
 
 ## generate a lot of different graphs
 ## show how they differ
