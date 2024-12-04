5d52c6a9129f2b8d331f27723a13c0392a4374fa
Ruhlen <ruhlensp@stu.cs.jmu.edu>
2024-12-04 11:00:11 -0500

    stuff

--- augmented_study/checkAccuracy.sh
+++ augmented_study/checkAccuracy.sh
@@ -1,14 +1,23 @@
 python genGraph.py
 
 echo "Starting Abby's Code now"
-python ../approx_solution/cs412_tsp_approx.py < graph > abby.txt
+
+# Run Abby code 6 times
+python ../approx_solution/cs412_tsp_approx.py < graph > abby.txt -s 50
+python ../approx_solution/cs412_tsp_approx.py < graph >> abby.txt -s 50
+python ../approx_solution/cs412_tsp_approx.py < graph >> abby.txt -s 50
+python ../approx_solution/cs412_tsp_approx.py < graph >> abby.txt -s 50
+python ../approx_solution/cs412_tsp_approx.py < graph >> abby.txt -s 50
+python ../approx_solution/cs412_tsp_approx.py < graph >> abby.txt -s 50
 
 echo "Starting Mateo's code now"
 python ../exact_solution/cs412_tsp_exact.py < graph > mateo.txt
 
 echo "Run Complete"
 
+# I keep Abby and Mateo's files separate so that I can keep track of things
+cat abby.txt mateo.txt > results
+
 echo "Plotting Results"
 
-## generate a lot of different graphs
-## show how they differ
+python plotResults.py < results
